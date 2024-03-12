8f2f6500a5cf23bf04cfa178ec95d1d94b996438
Andrew Gilewsky <andrew.gilewsky@gmail.com>
2024-03-12 18:31:03 +0000

    Custom rasterization.

--- vnavmesh/Debug/DebugNavmeshCustom.cs
+++ vnavmesh/Debug/DebugNavmeshCustom.cs
@@ -1,14 +1,21 @@
 ﻿using Dalamud.Interface.Utility.Raii;
+using DotRecast.Core;
+using DotRecast.Core.Numerics;
 using DotRecast.Detour;
+using DotRecast.Recast;
 using ImGuiNET;
 using Navmesh.NavVolume;
 using System;
+using System.Diagnostics;
+using System.Numerics;
 using System.Threading.Tasks;
 
 namespace Navmesh.Debug;
 
 class DebugNavmeshCustom : IDisposable
 {
+    private record struct HeightfieldComparison(float DurationOld, float DurationNew, bool Identical);
+
     // async navmesh builder
     public class AsyncBuilder : IDisposable
     {
@@ -115,6 +122,7 @@ class DebugNavmeshCustom : IDisposable
         public DebugContourSet? DrawContourSet;
         public DebugPolyMesh? DrawPolyMesh;
         public DebugPolyMeshDetail? DrawPolyMeshDetail;
+        public HeightfieldComparison? HFC;
 
         public void Dispose()
         {
@@ -132,6 +140,7 @@ class DebugNavmeshCustom : IDisposable
     private DebugDrawer _dd;
     private DebugGameCollision _coll;
     private DebugExtractedCollision? _drawExtracted;
+    private HeightfieldComparison? _globalHFC;
     private PerTile[,]? _debugTiles;
     private DebugDetourNavmesh? _drawNavmesh;
     private DebugVoxelMap? _debugVoxelMap;
@@ -191,31 +200,53 @@ class DebugNavmeshCustom : IDisposable
             if (n.Opened)
             {
                 _debugTiles ??= new PerTile[intermediates.NumTilesX, intermediates.NumTilesZ];
+                using (var ng = _tree.Node("Global"))
+                {
+                    if (ng.Opened)
+                    {
+                        _globalHFC ??= CompareAllHeightfields(_navmesh.Extractor!);
+                        _tree.LeafNode($"Old: {_globalHFC.Value.DurationOld:f3}");
+                        _tree.LeafNode($"New: {_globalHFC.Value.DurationNew:f3}");
+                        _tree.LeafNode($"Match: {_globalHFC.Value.Identical}");
+                    }
+                }
+
                 for (int z = 0; z < intermediates.NumTilesZ; ++z)
                 {
                     for (int x = 0; x < intermediates.NumTilesX; ++x)
                     {
-                        var inter = intermediates.Tiles[x, z];
-                        if (inter == null)
-                            continue;
-
                         using var nt = _tree.Node($"Tile {x}x{z}");
                         if (!nt.Opened)
                             continue;
 
+                        var inter = intermediates.Tiles[x, z];
                         var debug = _debugTiles[x, z] ??= new();
-                        debug.DrawSolidHeightfield ??= new(inter.Value.SolidHeightfield, _tree, _dd);
-                        debug.DrawSolidHeightfield.Draw();
-                        debug.DrawCompactHeightfield ??= new(inter.Value.CompactHeightfield, _tree, _dd);
-                        debug.DrawCompactHeightfield.Draw();
-                        debug.DrawContourSet ??= new(inter.Value.ContourSet, _tree, _dd);
-                        debug.DrawContourSet.Draw();
-                        debug.DrawPolyMesh ??= new(inter.Value.PolyMesh, _tree, _dd);
-                        debug.DrawPolyMesh.Draw();
-                        if (inter.Value.DetailMesh != null)
+                        if (inter != null)
                         {
-                            debug.DrawPolyMeshDetail ??= new(inter.Value.DetailMesh, _tree, _dd);
-                            debug.DrawPolyMeshDetail.Draw();
+                            debug.DrawSolidHeightfield ??= new(inter.Value.SolidHeightfield, _tree, _dd);
+                            debug.DrawSolidHeightfield.Draw();
+                            debug.DrawCompactHeightfield ??= new(inter.Value.CompactHeightfield, _tree, _dd);
+                            debug.DrawCompactHeightfield.Draw();
+                            debug.DrawContourSet ??= new(inter.Value.ContourSet, _tree, _dd);
+                            debug.DrawContourSet.Draw();
+                            debug.DrawPolyMesh ??= new(inter.Value.PolyMesh, _tree, _dd);
+                            debug.DrawPolyMesh.Draw();
+                            if (inter.Value.DetailMesh != null)
+                            {
+                                debug.DrawPolyMeshDetail ??= new(inter.Value.DetailMesh, _tree, _dd);
+                                debug.DrawPolyMeshDetail.Draw();
+                            }
+                        }
+
+                        using (var nhfc = _tree.Node("HF comparison"))
+                        {
+                            if (nhfc.Opened)
+                            {
+                                debug.HFC ??= CompareHeightfields(x, z, _navmesh.Extractor!);
+                                _tree.LeafNode($"Old: {debug.HFC.Value.DurationOld:f3}");
+                                _tree.LeafNode($"New: {debug.HFC.Value.DurationNew:f3}");
+                                _tree.LeafNode($"Match: {debug.HFC.Value.Identical}");
+                            }
                         }
                     }
                 }
@@ -231,6 +262,7 @@ class DebugNavmeshCustom : IDisposable
     {
         _drawExtracted?.Dispose();
         _drawExtracted = null;
+        _globalHFC = null;
         if (_debugTiles != null)
             foreach (var t in _debugTiles)
                 t?.Dispose();
@@ -240,5 +272,79 @@ class DebugNavmeshCustom : IDisposable
         _debugVoxelMap?.Dispose();
         _debugVoxelMap = null;
         _navmesh.Clear();
+    }
+
+    private HeightfieldComparison CompareHeightfields(int tx, int tz, SceneExtractor scene)
+    {
+        var telemetry = new RcContext();
+        var boundsMin = new Vector3(-1024);
+        var boundsMax = new RcVec3f(1024);
+        var numTilesXZ = _settings.NumTiles[0];
+        var tileWidth = (boundsMax.X - boundsMin.X) / numTilesXZ;
+        var tileHeight = (boundsMax.Z - boundsMin.Z) / numTilesXZ;
+        var walkableClimbVoxels = (int)MathF.Floor(_settings.AgentMaxClimb / _settings.CellHeight);
+        var walkableRadiusVoxels = (int)MathF.Ceiling(_settings.AgentRadius / _settings.CellSize);
+        var walkableNormalThreshold = _settings.AgentMaxSlopeDeg.Degrees().Cos();
+        var borderSizeVoxels = 3 + walkableRadiusVoxels;
+        var borderSizeWorld = borderSizeVoxels * _settings.CellSize;
+        var tileSizeXVoxels = (int)MathF.Ceiling(tileWidth / _settings.CellSize) + 2 * borderSizeVoxels;
+        var tileSizeZVoxels = (int)MathF.Ceiling(tileHeight / _settings.CellSize) + 2 * borderSizeVoxels;
+        var tileBoundsMin = new Vector3(boundsMin.X + tx * tileWidth, boundsMin.Y, boundsMin.Z + tz * tileHeight);
+        var tileBoundsMax = new Vector3(tileBoundsMin.X + tileWidth, boundsMax.Y, tileBoundsMin.Z + tileHeight);
+        tileBoundsMin.X -= borderSizeWorld;
+        tileBoundsMin.Z -= borderSizeWorld;
+        tileBoundsMax.X += borderSizeWorld;
+        tileBoundsMax.Z += borderSizeWorld;
+
+        var timer = Timer.Create();
+        var shfOld = new RcHeightfield(tileSizeXVoxels, tileSizeZVoxels, tileBoundsMin.SystemToRecast(), tileBoundsMax.SystemToRecast(), _settings.CellSize, _settings.CellHeight, borderSizeVoxels);
+        var rasterizerOld = new NavmeshRasterizer(shfOld, walkableNormalThreshold, walkableClimbVoxels, telemetry);
+        rasterizerOld.RasterizeOld(scene, true, true, true);
+        var dur1 = (float)timer.Value().TotalSeconds;
+
+        var shfNew = new RcHeightfield(tileSizeXVoxels, tileSizeZVoxels, tileBoundsMin.SystemToRecast(), tileBoundsMax.SystemToRecast(), _settings.CellSize, _settings.CellHeight, borderSizeVoxels);
+        var rasterizerNew = new NavmeshRasterizer(shfNew, walkableNormalThreshold, walkableClimbVoxels, telemetry);
+        rasterizerNew.Rasterize(scene, true, true, true);
+        var dur2 = (float)timer.Value().TotalSeconds;
+
+        bool identical = true;
+        int ispan = 0;
+        for (int z = 0; z < tileSizeZVoxels; ++z)
+        {
+            for (int x = 0; x < tileSizeXVoxels; ++x)
+            {
+                var so = shfOld.spans[ispan];
+                var sn = shfNew.spans[ispan];
+                while (so != 0 && sn != 0)
+                {
+                    ref var spanOld = ref shfOld.Span(so);
+                    ref var spanNew = ref shfNew.Span(sn);
+                    identical &= (spanOld.smin == spanNew.smin) && (spanOld.smax == spanNew.smax) && (spanOld.area == spanNew.area);
+                    so = spanOld.next;
+                    sn = spanNew.next;
+                }
+                identical &= (so == 0) && (sn == 0);
+                ispan++;
+            }
+        }
+        return new(dur1, dur2, identical);
+    }
+
+    private HeightfieldComparison CompareAllHeightfields(SceneExtractor scene)
+    {
+        float dur1 = 0, dur2 = 0;
+        bool identical = true;
+        var numTilesXZ = _settings.NumTiles[0];
+        for (int tz = 0; tz < numTilesXZ; ++tz)
+        {
+            for (int tx = 0; tx < numTilesXZ; ++tx)
+            {
+                var hfc = CompareHeightfields(tx, tz, scene);
+                dur1 += hfc.DurationOld;
+                dur2 += hfc.DurationNew;
+                identical &= hfc.Identical;
+            }
+        }
+        return new(dur1, dur2, identical);
     }
 }
--- vnavmesh/NavmeshRasterizer.cs
+++ vnavmesh/NavmeshRasterizer.cs
@@ -1,5 +1,6 @@
 ﻿using DotRecast.Core;
 using DotRecast.Recast;
+using System;
 using System.Numerics;
 
 namespace Navmesh;
@@ -7,9 +8,21 @@ namespace Navmesh;
 // utility to rasterize various meshes into a heightfield
 public class NavmeshRasterizer
 {
+    // cheap triangle-bbox test: if all 3 vertices are on the same side of the bbox plane, the triangle can be discarded
+    [Flags]
+    private enum OutFlags : byte
+    {
+        None = 0,
+        NegX = 1 << 0,
+        PosX = 1 << 1,
+        NegY = 1 << 2,
+        PosY = 1 << 3,
+        NegZ = 1 << 4,
+        PosZ = 1 << 5,
+    }
+
     private RcHeightfield _heightfield;
     private RcContext _telemetry;
-    private float[] _vertices = new float[3 * 256];
     private int _walkableClimbThreshold; // if two spans have maximums within this number of voxels, their area is 'merged' (higher is selected)
     private float _walkableNormalThreshold; // triangle is considered 'walkable' if it's world-space normal's Y coordinate is >= this
 
@@ -21,8 +34,153 @@ public class NavmeshRasterizer
         _walkableNormalThreshold = walkableNormalThreshold;
     }
 
-    public unsafe void Rasterize(SceneExtractor geom, bool includeTerrain, bool includeMeshes, bool includeAnalytic)
+    public void Rasterize(SceneExtractor geom, bool includeTerrain, bool includeMeshes, bool includeAnalytic)
     {
+        Span<Vector3> worldVertices = stackalloc Vector3[256];
+        Span<OutFlags> outFlags = stackalloc OutFlags[256];
+        Span<Vector3> clipRemainingZ = stackalloc Vector3[7];
+        Span<Vector3> clipRemainingX = stackalloc Vector3[7];
+        Span<Vector3> clipScratch = stackalloc Vector3[7];
+        Span<Vector3> clipCell = stackalloc Vector3[7];
+        float invCellXZ = 1.0f / _heightfield.cs;
+        float invCellY = 1.0f / _heightfield.ch;
+        float worldHeight = _heightfield.bmax.Y - _heightfield.bmin.Y;
+        foreach (var (name, mesh) in geom.Meshes)
+        {
+            var terrain = mesh.MeshFlags.HasFlag(SceneExtractor.MeshFlags.FromTerrain);
+            var analytic = mesh.MeshFlags.HasFlag(SceneExtractor.MeshFlags.FromAnalyticShape);
+            bool include = terrain ? includeTerrain : analytic ? includeAnalytic : includeMeshes;
+            if (!include)
+                continue;
+
+            foreach (var instance in mesh.Instances)
+            {
+                if (instance.WorldBounds.Max.X <= _heightfield.bmin.X || instance.WorldBounds.Max.Z <= _heightfield.bmin.Z || instance.WorldBounds.Min.X >= _heightfield.bmax.X || instance.WorldBounds.Min.Z >= _heightfield.bmax.Z)
+                    continue;
+
+                foreach (var part in mesh.Parts)
+                {
+                    // fill vertex buffer
+                    int iv = 0;
+                    foreach (var v in part.Vertices)
+                    {
+                        var w = instance.WorldTransform.TransformCoordinate(v);
+                        var f = OutFlags.None;
+                        if (w.X <= _heightfield.bmin.X) f |= OutFlags.NegX;
+                        if (w.X >= _heightfield.bmax.X) f |= OutFlags.PosX;
+                        if (w.Y <= _heightfield.bmin.Y) f |= OutFlags.NegY;
+                        if (w.Y >= _heightfield.bmax.Y) f |= OutFlags.PosY;
+                        if (w.Z <= _heightfield.bmin.Z) f |= OutFlags.NegZ;
+                        if (w.Z >= _heightfield.bmax.Z) f |= OutFlags.PosZ;
+                        worldVertices[iv] = w;
+                        outFlags[iv] = f;
+                        ++iv;
+                    }
+
+                    // TODO: move area-id calculations to extraction step + store indices in a form that allows using RasterizeTriangles()
+                    foreach (var p in part.Primitives)
+                    {
+                        var flags = (p.Flags & ~instance.ForceClearPrimFlags) | instance.ForceSetPrimFlags;
+                        if (flags.HasFlag(SceneExtractor.PrimitiveFlags.FlyThrough))
+                            continue; // TODO: rasterize to normal heightfield, can't do it right now, since we're using same heightfield for both mesh and volume
+
+                        if ((outFlags[p.V1] & outFlags[p.V2] & outFlags[p.V3]) != OutFlags.None)
+                            continue; // vertex is fully outside bounds, on one side of some plane
+
+                        var v1 = worldVertices[p.V1];
+                        var v2 = worldVertices[p.V2];
+                        var v3 = worldVertices[p.V3];
+                        var v12 = v2 - v1;
+                        var v13 = v3 - v1;
+                        var normal = Vector3.Normalize(Vector3.Cross(v12, v13));
+
+                        bool unwalkable = flags.HasFlag(SceneExtractor.PrimitiveFlags.ForceUnwalkable) || normal.Y < _walkableNormalThreshold;
+                        bool unlandable = flags.HasFlag(SceneExtractor.PrimitiveFlags.Unlandable);
+                        var areaId = unwalkable ? 0 : unlandable ? Navmesh.UnlandableAreaId : RcConstants.RC_WALKABLE_AREA;
+
+                        // prepare for clipping: while iterating over z, we'll keep the 'remaining polygon' in clipRemainingZ
+                        int numRemainingZ = 0;
+                        clipRemainingZ[numRemainingZ++] = v1;
+                        clipRemainingZ[numRemainingZ++] = v2;
+                        clipRemainingZ[numRemainingZ++] = v3;
+
+                        // calculate the footprint of the triangle on the grid's z-axis
+                        var (minZ, maxZ) = AxisMinMax(clipRemainingZ, numRemainingZ, 2);
+                        int z0 = (int)((minZ - _heightfield.bmin.Z) * invCellXZ); // TODO: not sure whether this is correct (round to 0 instead of floor...)
+                        int z1 = (int)((maxZ - _heightfield.bmin.Z) * invCellXZ);
+                        // note: no need to check for fully outside here, it was checked before
+                        z0 = Math.Clamp(z0, -1, _heightfield.height - 1); // use -1 rather than 0 to cut the polygon properly at the start of the tile
+                        z1 = Math.Clamp(z1, 0, _heightfield.height - 1);
+
+                        for (int z = z0; z <= z1; ++z)
+                        {
+                            if (numRemainingZ < 3)
+                                break;
+
+                            // clip polygon to 'row'
+                            var cellZMax = _heightfield.bmin.Z + (z + 1) * _heightfield.cs;
+                            int numRemainingX = SplitConvexPoly(clipRemainingZ, clipRemainingX, clipScratch, ref numRemainingZ, 2, cellZMax);
+
+                            // previous buffer is now new scratch
+                            var swapZ = clipRemainingZ;
+                            clipRemainingZ = clipScratch;
+                            clipScratch = swapZ;
+
+                            if (numRemainingX < 3 || z < 0)
+                                continue;
+
+                            // find x bounds of the row
+                            var (minX, maxX) = AxisMinMax(clipRemainingX, numRemainingX, 0);
+                            int x0 = (int)((minX - _heightfield.bmin.X) * invCellXZ); // TODO: not sure whether this is correct (round to 0 instead of floor...)
+                            int x1 = (int)((maxX - _heightfield.bmin.X) * invCellXZ);
+                            if (x1 < 0 || x0 >= _heightfield.width)
+                                continue;
+                            x0 = Math.Clamp(x0, -1, _heightfield.width - 1);
+                            x1 = Math.Clamp(x1, 0, _heightfield.width - 1);
+
+                            for (int x = x0; x <= x1; ++x)
+                            {
+                                if (numRemainingX < 3)
+                                    break;
+
+                                // clip polygon to 'column'
+                                var cellXMax = _heightfield.bmin.X + (x + 1) * _heightfield.cs;
+                                int numCell = SplitConvexPoly(clipRemainingX, clipCell, clipScratch, ref numRemainingX, 0, cellXMax);
+
+                                // previous buffer is now new scratch
+                                var swapX = clipRemainingX;
+                                clipRemainingX = clipScratch;
+                                clipScratch = swapX;
+
+                                if (numCell < 3 || x < 0)
+                                    continue;
+
+                                // find y bounds of the cell (TODO: this can probably be slightly simplified)
+                                var (minY, maxY) = AxisMinMax(clipCell, numCell, 1);
+                                minY -= _heightfield.bmin.Y;
+                                maxY -= _heightfield.bmin.Y;
+                                if (maxY < 0 || minY > worldHeight)
+                                    continue;
+                                minY = Math.Max(minY, 0);
+                                maxY = Math.Min(maxY, worldHeight);
+                                int y0 = Math.Clamp((int)MathF.Floor(minY * invCellY), 0, RcConstants.RC_SPAN_MAX_HEIGHT);
+                                int y1 = Math.Clamp((int)MathF.Ceiling(maxY * invCellY), y0 + 1, RcConstants.RC_SPAN_MAX_HEIGHT);
+
+                                RcRasterizations.AddSpan(_heightfield, x, z, y0, y1, areaId, _walkableClimbThreshold);
+
+                                // TODO: find intersection with ray along -y through the center of the cell, combine with normal.Y sign and add to per-cell list of enter/exit points
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    // TODO: remove after i'm confident in my replacement code
+    public void RasterizeOld(SceneExtractor geom, bool includeTerrain, bool includeMeshes, bool includeAnalytic)
+    {
+        float[] vertices = new float[3 * 256];
         foreach (var (name, mesh) in geom.Meshes)
         {
             var terrain = mesh.MeshFlags.HasFlag(SceneExtractor.MeshFlags.FromTerrain);
@@ -43,9 +201,9 @@ public class NavmeshRasterizer
                     foreach (var v in part.Vertices)
                     {
                         var w = inst.WorldTransform.TransformCoordinate(v);
-                        _vertices[iv++] = w.X;
-                        _vertices[iv++] = w.Y;
-                        _vertices[iv++] = w.Z;
+                        vertices[iv++] = w.X;
+                        vertices[iv++] = w.Y;
+                        vertices[iv++] = w.Z;
                     }
 
                     // TODO: move area-id calculations to extraction step + store indices in a form that allows using RasterizeTriangles()
@@ -58,9 +216,9 @@ public class NavmeshRasterizer
                         bool walkable = !flags.HasFlag(SceneExtractor.PrimitiveFlags.ForceUnwalkable);
                         if (walkable)
                         {
-                            var v1 = CachedVertex(p.V1);
-                            var v2 = CachedVertex(p.V2);
-                            var v3 = CachedVertex(p.V3);
+                            var v1 = CachedVertex(vertices, p.V1);
+                            var v2 = CachedVertex(vertices, p.V2);
+                            var v3 = CachedVertex(vertices, p.V3);
                             var v12 = v2 - v1;
                             var v13 = v3 - v1;
                             var normal = Vector3.Normalize(Vector3.Cross(v12, v13));
@@ -68,16 +226,81 @@ public class NavmeshRasterizer
                         }
 
                         var areaId = !walkable ? 0 : flags.HasFlag(SceneExtractor.PrimitiveFlags.Unlandable) ? Navmesh.UnlandableAreaId : RcConstants.RC_WALKABLE_AREA;
-                        RcRasterizations.RasterizeTriangle(_telemetry, _vertices, p.V1, p.V2, p.V3, areaId, _heightfield, _walkableClimbThreshold);
+                        RcRasterizations.RasterizeTriangle(_telemetry, vertices, p.V1, p.V2, p.V3, areaId, _heightfield, _walkableClimbThreshold);
                     }
                 }
             }
         }
     }
 
-    private Vector3 CachedVertex(int i)
+    private static Vector3 CachedVertex(ReadOnlySpan<float> vertices, int i)
     {
         var offset = 3 * i;
-        return new(_vertices[offset], _vertices[offset + 1], _vertices[offset + 2]);
+        return new(vertices[offset], vertices[offset + 1], vertices[offset + 2]);
+    }
+
+    private static (float min, float max) AxisMinMax(Span<Vector3> vertices, int count, int axis)
+    {
+        float min = vertices[0][axis];
+        float max = min;
+        for (int i = 1; i < count; ++i)
+        {
+            float v = vertices[i][axis];
+            min = Math.Min(min, v);
+            max = Math.Max(max, v);
+        }
+        return (min, max);
+    }
+
+    // split a convex polygon along one of the axes
+    // polygon 'smaller' than axis offset is to be processed next, rest is considered to be a leftover to process on the next iteration
+    // count on input contains num vertices in src buffer, on output num vertices in remaining buffer
+    private static int SplitConvexPoly(Span<Vector3> src, Span<Vector3> dest, Span<Vector3> remaining, ref int count, int axis, float axisOffset)
+    {
+        Span<float> axisDelta = stackalloc float[count];
+        for (int i = 0; i < count; ++i)
+            axisDelta[i] = axisOffset - src[i][axis];
+
+        int cDest = 0, cRem = 0;
+        var dPrev = axisDelta[count - 1];
+        var vPrev = src[count - 1];
+        for (int i = 0; i < count; ++i)
+        {
+            var dCurr = axisDelta[i];
+            var vCurr = src[i];
+            if ((dCurr >= 0) != (dPrev >= 0))
+            {
+                // two vertices are on the different sides of the separating axis
+                float s = dPrev / (dPrev - dCurr);
+                dest[cDest++] = remaining[cRem++] = vPrev + (vCurr - vPrev) * s;
+
+                // add the i'th point to the right polygon; do NOT add points that are on the dividing line since these were already added above
+                if (dCurr > 0)
+                    dest[cDest++] = vCurr;
+                else if (dCurr < 0)
+                    remaining[cRem++] = vCurr;
+            }
+            else
+            {
+                // add the i'th point to the right polygon; addition is done even for points on the dividing line
+                if (dCurr > 0)
+                {
+                    dest[cDest++] = vCurr;
+                }
+                else if (dCurr < 0)
+                {
+                    remaining[cRem++] = vCurr;
+                }
+                else
+                {
+                    dest[cDest++] = vCurr;
+                    remaining[cRem++] = vCurr;
+                }
+            }
+            dPrev = dCurr;
+            vPrev = vCurr;
+        }
+        count = cRem;
+        return cDest;
     }
 }

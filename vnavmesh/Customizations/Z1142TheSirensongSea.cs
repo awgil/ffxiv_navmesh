fb9eda57fe4e05f97d21e9904bd3bba7743e7d9b
Adam Bennett <adam-bennett@live.com>
2024-04-08 11:43:50 -0700

    Added The Praetorium and The Sirensong Sea Customizations Also made DTSucces.Failure return blank List for validation purposes

---
+++ vnavmesh/Customizations/Z1142TheSirensongSea.cs
@@ -0,0 +1,15 @@
+﻿namespace Navmesh.Customizations;
+
+[CustomizationTerritory(1142)]
+class Z1142TheSirensongSea : NavmeshCustomization
+{
+    public override int Version => 1;
+
+    public Z1142TheSirensongSea()
+    {
+        Settings.Filtering -= NavmeshSettings.Filter.LedgeSpans; // this allows mesh to go down the ramp from the magitek armor, and its ok in dungeons
+                                                                 // because non traversable ledges will have Plane colliders which will not allow the
+                                                                 // mesh to overhang anyways even if Rasterization is conservative
+                                                                 // (because being conservative does not ignore Plane Colliders)
+    }
+}

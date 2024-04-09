6ec3fe22cac74a0e1af417b53945b1aad801ddd8
Adam Bennett <adam-bennett@live.com>
2024-04-09 11:27:40 -0700

    Added: Z1042TheStoneVigil Z130UldahStepsofNald
    and UnwalkableMesh in SceneExtractor

---
+++ vnavmesh/Customizations/Z1042TheStoneVigil.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Navmesh.Customizations;
+
+[CustomizationTerritory(1042)]
+class Z1042TheStoneVigil : NavmeshCustomization
+{
+    public override int Version => 1;
+    
+    public override void CustomizeScene(SceneExtractor scene) 
+    {
+        //Force all Primitives of Mesh as unwalkable
+        if (scene.Meshes.TryGetValue("bg/ffxiv/roc_r1/dun/r1d1/collision/r1d1_b1_sas03.pcb", out var mesh))
+            scene.Meshes["bg/ffxiv/roc_r1/dun/r1d1/collision/r1d1_b1_sas03.pcb"] = scene.UnwalkableMesh(mesh);
+    }
+    
+    public Z1042TheStoneVigil()
+    {
+    }
+}
---
+++ vnavmesh/Customizations/Z130UldahStepsofNald.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Navmesh.Customizations;
+
+[CustomizationTerritory(130)]
+class Z130UldahStepsofNald : NavmeshCustomization
+{
+    public override int Version => 1;
+    
+    public override void CustomizeScene(SceneExtractor scene) 
+    {
+        //Force all Primitives of Mesh as unwalkable
+        if (scene.Meshes.TryGetValue("bg/ffxiv/wil_w1/twn/common/collision/w1t0_f0_kadn1.pcb", out var mesh))
+            scene.Meshes["bg/ffxiv/wil_w1/twn/common/collision/w1t0_f0_kadn1.pcb"] = scene.UnwalkableMesh(mesh);
+    }
+    
+    public Z130UldahStepsofNald()
+    {
+    }
+}

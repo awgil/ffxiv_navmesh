aa319bdeaa06ce8b3b2e754655460e884a51f91e
Adam Bennett <adam-bennett@live.com>
2024-07-26 21:14:31 -0700

    Added Limsa Upper Customization to avoid Stuck on pillars

---
+++ vnavmesh/Customizations/Z0128LimsaLominsaUpperDecks.cs
@@ -0,0 +1,12 @@
+﻿namespace Navmesh.Customizations;
+
+[CustomizationTerritory(128)]
+class Z0128LimsaLominsaUpperDecks : NavmeshCustomization
+{
+    public override int Version => 1;
+
+    public Z0128LimsaLominsaUpperDecks()
+    {
+        Settings.AgentRadius = 0.75f;
+    }
+}

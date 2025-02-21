a3cbe517c3e1f952262e73beed11ad7a09f6f89a
xanunderscore <149614526+xanunderscore@users.noreply.github.com>
2025-02-20 19:09:42 -0500

    central highlands customization

---
+++ vnavmesh/Customizations/Z0155CoerthasCentralHighlands.cs
@@ -0,0 +1,13 @@
+﻿namespace Navmesh.Customizations;
+
+[CustomizationTerritory(155)]
+class Z0155CoerthasCentralHighlands : NavmeshCustomization
+{
+    public override int Version => 1;
+
+    public Z0155CoerthasCentralHighlands()
+    {
+        // the second staircase inside the building in Whitebrim Front is not actually connected directly to the landing, but can be walked on regardless
+        Settings.AgentMaxClimb = 0.75f;
+    }
+}

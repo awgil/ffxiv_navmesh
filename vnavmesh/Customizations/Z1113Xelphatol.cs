d1751572fd89da51a177f66104856a79334595d6
xanunderscore <149614526+xanunderscore@users.noreply.github.com>
2025-02-21 18:26:58 -0500

    xelphatol drawbridge fix

---
+++ vnavmesh/Customizations/Z1113Xelphatol.cs
@@ -0,0 +1,14 @@
+﻿namespace Navmesh.Customizations;
+
+[CustomizationTerritory(1113)]
+class Z1113Xelphatol : NavmeshCustomization
+{
+    public override int Version => 1;
+
+    public override void CustomizeScene(SceneExtractor scene)
+    {
+        // remove raised drawbridges
+        if (scene.Meshes.TryGetValue("<box>", out var boxes))
+            boxes.Instances.RemoveAll(inst => inst.Id is 0x616AB407000000 or 0x616AE407000000);
+    }
+}

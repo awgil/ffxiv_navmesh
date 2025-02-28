a7a083978df45d2193a56c107fa823dd21dbf824
xanunderscore <149614526+xanunderscore@users.noreply.github.com>
2025-02-28 16:02:28 -0500

    add other lift in arf

--- vnavmesh/Customizations/Z1110AetherochemicalResearchFacility.cs
+++ vnavmesh/Customizations/Z1110AetherochemicalResearchFacility.cs
@@ -3,15 +3,17 @@
 [CustomizationTerritory(1110)]
 class Z1110AetherochemicalResearchFacility : NavmeshCustomization
 {
-    public override int Version => 2;
+    public override int Version => 3;
 
     public override void CustomizeScene(SceneExtractor scene)
     {
         // colliders blocking lifts
         scene.Meshes.Remove("<box>");
 
-        // the final lift actually moves when being used, so if the client triggers a rebuild at the end of the dungeon for whatever reason, it will break mesh connectivity (for subsequent runs)
+        // the lifts in the final room move when being used, so if the client triggers a rebuild at the end of the dungeon for whatever reason, it will break mesh connectivity (for subsequent runs)
         var (transform, aabb) = GenerateTransformAABB(new(10, 1, 10), new(221.229f, -60, 95f));
         scene.Meshes["<cylinder>"].Instances.Add(new(0xbaadf00d00000001ul, transform, aabb, default, default));
+        (transform, aabb) = GenerateTransformAABB(new(10, 1, 10), new(195f, -29f, 196f));
+        scene.Meshes["<cylinder>"].Instances.Add(new(0xbaadf00d00000002ul, transform, aabb, default, default));
     }
 }

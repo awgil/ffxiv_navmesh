070f5388a0304bf96aa5f4fc42403ffc30aa74be
xanunderscore <149614526+xanunderscore@users.noreply.github.com>
2025-03-11 00:11:10 -0400

    a

---
+++ vnavmesh/Customizations/Z1044ThePraetorium.cs
@@ -0,0 +1,14 @@
+﻿namespace Navmesh.Customizations;
+
+[CustomizationTerritory(1044)]
+class Z1044ThePraetorium : NavmeshCustomization
+{
+    public override int Version => 3;
+
+    public Z1044ThePraetorium()
+    {
+        // allow connection between higher and lower section of broken ramp right after the magitek armor
+        Settings.Filtering -= NavmeshSettings.Filter.LedgeSpans;
+    }
+}
+

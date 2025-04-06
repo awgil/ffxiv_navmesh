ff88f56af623467dc0faf90ac181c549a1fe6e96
xanunderscore <149614526+xanunderscore@users.noreply.github.com>
2025-04-06 12:11:59 -0400

    new zone customizations

---
+++ vnavmesh/Customizations/Z0152EastShroud.cs
@@ -0,0 +1,12 @@
+﻿namespace Navmesh.Customizations;
+
+[CustomizationTerritory(152)]
+internal class Z0152EastShroud : NavmeshCustomization
+{
+    public override int Version => 1;
+
+    public override void CustomizeScene(SceneExtractor scene)
+    {
+        scene.InsertCylinderCollider(new System.Numerics.Vector3(2, 2, 2), new(-40, -8, 225), SceneExtractor.PrimitiveFlags.Unlandable);
+    }
+}

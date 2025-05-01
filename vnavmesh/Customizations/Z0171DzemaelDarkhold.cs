129072e4add8ead08b151528760439b5e7a0880f
xanunderscore <149614526+xanunderscore@users.noreply.github.com>
2025-05-01 14:58:35 -0400

    who wrote this

--- vnavmesh/Customizations/Z0171DzemaelDarkhold.cs
+++ vnavmesh/Customizations/Z0171DzemaelDarkhold.cs
@@ -3,23 +3,12 @@
 [CustomizationTerritory(171)]
 class Z0171DzemaelDarkhold : NavmeshCustomization
 {
-    public override int Version => 1;
+    public override int Version => 2;
 
     public override void CustomizeScene(SceneExtractor scene)
     {
-        //change worldbounds of all instances (These colliders are there just too low to make that mesh connection at the doors, increasing there Y by 1 fixes this)
-        if (scene.Meshes.TryGetValue("bg/ffxiv/roc_r1/rad/r1r1/collision/r1r1_a1_dor4.pcb", out var mesh))
-        {
-            foreach (var instance in mesh.Instances)
-            {
-                instance.WorldBounds.Min.Y += 1f;
-                instance.WorldBounds.Max.Y += 1f;
-            }
-        }
-    }
-
-    public Z0171DzemaelDarkhold()
-    {
-        Settings.Partitioning = DotRecast.Recast.RcPartition.MONOTONE;
+        foreach (var (key, mesh) in scene.Meshes)
+            if (key.StartsWith("bg/ffxiv/roc_r1/rad/r1r1/collision/r1r1_a1_dor"))
+                mesh.Instances.Clear();
     }
 }

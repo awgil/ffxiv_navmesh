e1fb670abf2c53de70b7dcd60291fc8c543d630b
xanunderscore <149614526+xanunderscore@users.noreply.github.com>
2025-06-13 16:11:06 -0400

    OC sw tower fix

---
+++ vnavmesh/Customizations/Z1252OccultCrescent.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Navmesh.Customizations;
+
+[CustomizationTerritory(1252)]
+internal class Z1252OccultCrescent : NavmeshCustomization
+{
+    public override int Version => 1;
+
+    public override void CustomizeScene(SceneExtractor scene)
+    {
+        if (scene.Meshes.TryGetValue("bg/ex5/03_ocn_o6/btl/o6b1/collision/o6b1_a5_stc02.pcb", out var mesh))
+        {
+            // bottom stair of second-tier staircase around SW tower is too steep at 51 degrees, even if agent max-climb is set to 55, probably because of rasterization bs, extend it outward by 1y to make slope more gradual
+            ref var part0 = ref CollectionsMarshal.AsSpan(mesh.Parts)[221];
+            var verts = CollectionsMarshal.AsSpan(part0.Vertices);
+            verts[8].X += 1;
+            verts[16].X += 1;
+        }
+    }
+}

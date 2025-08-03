8c0d80c9324d8d63c6ff003e4547857f324d2844
xanunderscore <149614526+xanunderscore@users.noreply.github.com>
2025-08-03 11:33:31 -0400

    island sanctuary rebuild

---
+++ vnavmesh/Customizations/Z1055IslandSanctuary.cs
@@ -0,0 +1,8 @@
+﻿namespace Navmesh.Customizations;
+
+[CustomizationTerritory(1055)]
+internal class Z1055IslandSanctuary : NavmeshCustomization
+{
+    // empty customization to force rebuild for existing users
+    public override int Version => 1;
+}

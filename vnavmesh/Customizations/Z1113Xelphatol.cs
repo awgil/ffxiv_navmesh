010f5b4624c728ffd1cbe48a3b79f1409928e313
xanunderscore <149614526+xanunderscore@users.noreply.github.com>
2025-11-27 17:45:17 -0500

    remove obsolete customizers

--- vnavmesh/Customizations/Z1113Xelphatol.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Navmesh.Customizations;
-
-[CustomizationTerritory(1113)]
-class Z1113Xelphatol : NavmeshCustomization
-{
-    public override int Version => 2;
-
-    public override bool FilterObject(InstanceWithMesh inst) => inst.Mesh.Path != "<box>";
-}

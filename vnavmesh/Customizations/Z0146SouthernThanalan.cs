ea2d0258f35d7b130f7215d056f5a1c2faaac420
xanunderscore <149614526+xanunderscore@users.noreply.github.com>
2025-12-07 12:10:37 -0500

    force rebuild

---
+++ vnavmesh/Customizations/Z0146SouthernThanalan.cs
@@ -0,0 +1,8 @@
+﻿namespace Navmesh.Customizations;
+
+[CustomizationTerritory(146)]
+internal class Z0146SouthernThanalan : NavmeshCustomization
+{
+    // force rebuild (gates)
+    public override int Version => 1;
+}

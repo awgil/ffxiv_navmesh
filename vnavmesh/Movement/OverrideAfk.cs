95a2eb050b81610ff28de8168c64524ff842e227
xanunderscore <149614526+xanunderscore@users.noreply.github.com>
2025-12-19 11:27:44 -0500

    api14

--- vnavmesh/Movement/OverrideAfk.cs
+++ vnavmesh/Movement/OverrideAfk.cs
@@ -10,6 +10,6 @@ internal unsafe static class OverrideAFK
         module->AfkTimer = 0;
         module->ContentInputTimer = 0;
         module->InputTimer = 0;
-        module->Unk1C = 0;
+        // module->Unk1C = 0;
     }
 }

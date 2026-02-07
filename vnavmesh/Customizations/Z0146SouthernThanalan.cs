3f5f3a3ee7a90f700bd45253bc38a30356ab6dc6
xanunderscore <149614526+xanunderscore@users.noreply.github.com>
2026-02-07 13:58:12 -0500

    fix two zones with random unlandable objects

--- vnavmesh/Customizations/Z0146SouthernThanalan.cs
+++ vnavmesh/Customizations/Z0146SouthernThanalan.cs
@@ -3,11 +3,16 @@
 [CustomizationTerritory(146)]
 internal class Z0146SouthernThanalan : NavmeshCustomization
 {
-    public override int Version => 3;
+    public override int Version => 4;
 
     public override void CustomizeScene(SceneExtractor scene)
     {
         if (scene.Meshes.TryGetValue("<box>", out var mesh))
             mesh.Instances.RemoveAll(i => i.Material == 0x206406);
+
+        // the ground directly in front of the bridge next to the amalj'aa camp has two triangles that cannot be landed on
+        if (scene.Meshes.TryGetValue("bg/ffxiv/wil_w1/fld/w1f4/collision/tr1610.pcb", out var mesh2))
+            foreach (var inst in mesh2.Instances)
+                inst.ForceClearPrimFlags |= SceneExtractor.PrimitiveFlags.Unlandable;
     }
 }
---
+++ vnavmesh/Customizations/Z0613RubySea.cs
@@ -0,0 +1,15 @@
+﻿using DotRecast.Detour;
+using Navmesh;
+using System.Collections.Generic;
+
+namespace vnavmesh.Customizations;
+
+[CustomizationTerritory(613)]
+internal class Z0613RubySea : NavmeshCustomization
+{
+    public override void CustomizeMesh(DtNavMesh mesh, List<uint> festivalLayers)
+    {
+        // the tunnel into the island containing tamamizu has some floor that is unlandable
+        LinkPoints(mesh, new(643.7f, 3.4f, -58.9f), new(636.6f, 3.9f, -63.3f));
+    }
+}
